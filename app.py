2d1d4198462ecaf998cce03afb9957fd1869c08f
lgdsbrand <legendsbrandcompany@gmail.com>
2025-08-26 16:22:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,1 @@
-import streamlit as st, sys, platform
-st.set_page_config(page_title="NFL Hello", layout="wide")
-st.title("🏈 NFL Test App")
-st.write("✅ App is alive.")
-st.write("Python:", sys.version)
-st.write("Platform:", platform.platform())
-st.link_button("⬅️ Back to Home", "https://lineupwire.com")
+
